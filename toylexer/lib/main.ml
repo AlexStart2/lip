99491869913238d6d3253d7c4ac6d0add2245140
AlexStart2 <fairytailend2003@gmail.com>
2024-10-23 17:50:05 +0300

    task 1

--- toylexer/lib/main.ml
+++ toylexer/lib/main.ml
@@ -24,4 +24,34 @@ let string_of_frequencies fl =
   List.fold_left (fun s (t,n) -> s ^ ((string_of_token t) ^ " -> " ^ string_of_int n ^ "\n")) "" fl
 
 (* frequency : int -> 'a list -> ('a * int) list *)
-let frequency _ _ = failwith("TODO")
+
+
+let rec take_first_n_elements lst n =
+  match lst, n with
+  | _, 0 -> []  (* If n is 0, return an empty list *)
+  | [], _ -> []  (* If the list is empty, return an empty list *)
+  | hd :: tl, _ -> hd :: take_first_n_elements tl (n - 1)  (* Take the head and continue *)
+
+
+let freq tl =
+  let a =
+    List.fold_left(fun result elem -> 
+      if List.exists (fun (first, _) -> first = elem) result then
+        result
+      else
+        let count = List.length (List.find_all ((=) elem) tl) in
+        List.append([elem, count]) result) [] tl
+      in
+      List.sort (fun (_, a) (_, b) -> compare b a) a
+
+let frequency n tl = 
+  take_first_n_elements (freq tl) n
+
+
+  (* match List.hd tl with 
+  | [] -> result
+  | a -> let index = List.find_index(fun (_, second) -> second = a ) result in if (index = None && List.length result <n) then
+    List.append([1, a]) result else *)
+
+    (* let elem = List.hd(tl) in
+    let index = List.find_index(fun (_, second) -> second = elem ) result in *)
